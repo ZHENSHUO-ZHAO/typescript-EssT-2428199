680eec4359533dc1a404ad26536afcf41269e7de
ZHENSHUO ZHAO <s3992237@student.rmit.edu.au>
2025-09-26 20:25:58 +1000

    My practice

--- app.ts
+++ app.ts
@@ -1,27 +1,47 @@
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
-]
-
-function addTodoItem(todo) {
-    const id = getNextId(todoItems)
-
-    const newTodo = {
-        id,
-        title: todo,
-        status: "todo",
-    }
-
-    todoItems.push(newTodo)
-
-    return newTodo
+enum Status {
+  done = "done",
+  inProgress = "in-progress",
+  todo = "todo",
 }
 
-function getNextId(items) {
-    return items.reduce((max, x) => x.id > max ? max : x.id, 0) + 1
+interface TodoItem {
+  id: number;
+  title: string;
+  status: Status;
+  completedOn?: Date;
 }
 
-const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
+const todoItems: TodoItem[] = [
+  {
+    id: 1,
+    title: "Learn HTML",
+    status: Status.done,
+    completedOn: new Date("2021-09-11"),
+  },
+  { id: 2, title: "Learn TypeScript", status: Status.inProgress },
+  { id: 3, title: "Write the best app in the world", status: Status.todo },
+];
 
-console.log(JSON.stringify(newTodo))
+function addTodoItem(todo: string): TodoItem {
+  const id = getNextId(todoItems);
+
+  const newTodo = {
+    id,
+    title: todo,
+    status: Status.todo,
+  };
+
+  todoItems.push(newTodo);
+
+  return newTodo;
+}
+
+function getNextId<T extends { id: number }>(items: T[]): number {
+  return items.reduce((max, x) => (x.id > max ? max : x.id), 0) + 1;
+}
+
+const newTodo = addTodoItem(
+  "Buy lots of stuff with all the money we make from the app"
+);
+
+console.log(JSON.stringify(newTodo));
